ce04d7e860704863f7c77a982503f35f0c8ffb54
Dishu <dishanksharma644@gmail.com>
2026-07-02 23:40:01 +0530

    REfactor health point using MVC architecture

---
+++ backend/src/controllers/healthController.js
@@ -0,0 +1,4 @@
+const getHealth=(req,res)=>{
+    res.json({message:"CP TRACKER backend is running"});
+};
+module.exports={getHealth,};
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,6 @@
 const express=require("express");
 const app=express();
 const PORT=3000;
-app.get("/",(req,res)=>{res.json({message:"CP TRACKER backend is running"});});
+const routes=require("./routes")
+app.use("/api",routes);
 app.listen(PORT,()=>{console.log(`Server is running on port:${PORT}`)});
