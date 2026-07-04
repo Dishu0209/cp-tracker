e693fcadd010becca23aae0b445ab9244c0230b2
Dishu <dishanksharma644@gmail.com>
2026-07-04 16:01:06 +0530

    Add user analytics endpoint

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -1,117 +1,163 @@
- const { fetchUserInfo, fetchRatingHistory,fetchSubmission } = require("../services/codeforcesServices");
-const getUser= async(req,res)=>{
-    try{
-    const handle=req.params.handle;
-    
-   const data = await fetchUserInfo(handle);
-   
-    
-    if(data.status!=="OK")
-    {
-       return res.status(404).json({
-            success:false,
-            message:"NO USER FOUND"
-        })
+const {
+  fetchUserInfo,
+  fetchRatingHistory,
+  fetchSubmission,
+} = require("../services/codeforcesServices");
+const getUser = async (req, res) => {
+  try {
+    const handle = req.params.handle;
+
+    const data = await fetchUserInfo(handle);
+
+    if (data.status !== "OK") {
+      return res.status(404).json({
+        success: false,
+        message: "NO USER FOUND",
+      });
     }
-    const user=data.result[0];
+    const user = data.result[0];
     console.log(data);
 
     res.status(200).json({
-         success:true,
-       handle: user.handle,
-    rating: user.rating,
-    maxRating: user.maxRating,
-    rank: user.rank,
-    maxRank: user.maxRank,
-    avatar: user.avatar,
-    organization: user.organization
+      success: true,
+      handle: user.handle,
+      rating: user.rating,
+      maxRating: user.maxRating,
+      rank: user.rank,
+      maxRank: user.maxRank,
+      avatar: user.avatar,
+      organization: user.organization,
     });
-}
-catch(error)
-{
+  } catch (error) {
     console.error(error);
-    
+
     return res.status(500).json({
-        success:false,
-        message:"Internal Server Error"
+      success: false,
+      message: "Internal Server Error",
     });
-}
+  }
 };
 const getRatingHistory = async (req, res) => {
-     try{
-    const handle=req.params.handle;
-    const data=await fetchRatingHistory(handle);
-    
-    if(data.status==="OK")
-   { console.log (data);
-    const ratinghistory=data.result.map((contest)=>{
+  try {
+    const handle = req.params.handle;
+    const data = await fetchRatingHistory(handle);
+
+    if (data.status === "OK") {
+      console.log(data);
+      const ratinghistory = data.result.map((contest) => {
         return {
-            name:contest.contestName,
-            rank:contest.rank,
-            time:contest.ratingUpdateTimeSeconds,
-            oldRating:contest.oldRating,
-            newRating:contest.newRating
-
-        }
-    });
-    return res.status(200).json({success:true,
-        data:ratinghistory});}
-    else
-    {
-        return res.status(404).json({
-            success:false,
-            message:"USER NOT FOUND"
-        });
+          name: contest.contestName,
+          rank: contest.rank,
+          time: contest.ratingUpdateTimeSeconds,
+          oldRating: contest.oldRating,
+          newRating: contest.newRating,
+        };
+      });
+      return res.status(200).json({ success: true, data: ratinghistory });
+    } else {
+      return res.status(404).json({
+        success: false,
+        message: "USER NOT FOUND",
+      });
     }
-}
-catch(error)
-{
+  } catch (error) {
     return res.status(500).json({
-        success:false,
-        message:"Internal Server Error"
-    })
-}
-
-};
-const getSubmission=async(req,res)=>{
-    try
-{const handle=req.params.handle;
-const data=await fetchSubmission(handle);
-if(data.status!=="OK")
-{
-    return res.status(404).json({
-        success:false,
-        message:"USER NOT FOUND"
+      success: false,
+      message: "Internal Server Error",
     });
-}
-else
-    {
-        const submissions = data.result.map((submission) => {
-    return {
-        id: submission.id,
-        time: submission.creationTimeSeconds,
-        contestId: submission.problem.contestId,
-        index: submission.problem.index,
-        name: submission.problem.name,
-        rating: submission.problem.rating,
-        tags: submission.problem.tags,
-        language: submission.programmingLanguage,
-        verdict: submission.verdict,
-        participantType: submission.author.participantType
-    };
-});
-
-return res.status(200).json({
-    success: true,
-    data: submissions
-});
-    }}
-    catch(error)
-    {
-        return res.status(500).json({
-            success:false,
-            message:"Internal Server Error"
-        })
-    }
+  }
 };
-module.exports={getUser,getRatingHistory,getSubmission};
+const getSubmission = async (req, res) => {
+  try {
+    const handle = req.params.handle;
+    const data = await fetchSubmission(handle);
+    if (data.status !== "OK") {
+      return res.status(404).json({
+        success: false,
+        message: "USER NOT FOUND",
+      });
+    } else {
+      const submissions = data.result.map((submission) => {
+        return {
+          id: submission.id,
+          time: submission.creationTimeSeconds,
+          contestId: submission.problem.contestId,
+          index: submission.problem.index,
+          name: submission.problem.name,
+          rating: submission.problem.rating,
+          tags: submission.problem.tags,
+          language: submission.programmingLanguage,
+          verdict: submission.verdict,
+          participantType: submission.author.participantType,
+        };
+      });
+
+      return res.status(200).json({
+        success: true,
+        data: submissions,
+      });
+    }
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: "Internal Server Error",
+    });
+  }
+};
+const getanalytics = async (req, res) => {
+  try {
+    const handle = req.params.handle;
+    const data = await fetchSubmission(handle);
+    if (data.status !== "OK") {
+      return res.status(404).json({
+        success: false,
+        message: "USER NOT FOUND",
+      });
+    } else {
+      const totalSubmission = data.result.length;
+      let acceptedSubmissions = 0;
+      const solvedProblems = new Map();
+      for (const submission of data.result) {
+        if (submission.verdict === "OK") {
+          acceptedSubmissions++;
+
+          const problemKey = `${submission.problem.contestId}-${submission.problem.index}`;
+
+          solvedProblems.set(problemKey, submission.problem);
+        }
+      }
+      const tagDistribution = new Map();
+      const uniqueSolvedProblems = solvedProblems.size;
+      const ratingDistribution = new Map();
+      for (const [problemKey, problem] of solvedProblems) {
+        const rating = problem.rating ?? "Unrated";
+        ratingDistribution.set(
+          rating,
+          (ratingDistribution.get(rating) || 0) + 1,
+        );
+        for (const tag of problem.tags) {
+    tagDistribution.set(
+        tag,
+        (tagDistribution.get(tag) || 0) + 1
+    );
+}
+Object.fromEntries(ratingDistribution);
+Object.fromEntries(tagDistribution);
+      }
+      return res.status(200).json({
+        success:true,
+        totalSubmission:totalSubmission,
+        totalaccepted:acceptedSubmissions,
+        uniqueproblems:uniqueSolvedProblems,
+        ratingDistribution:Object.fromEntries(ratingDistribution),
+        tagDistribution:Object.fromEntries(tagDistribution)
+      });
+    }
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: "Internal Server Error",
+    });
+  }
+};
+module.exports = { getUser, getRatingHistory, getSubmission, getanalytics };
--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,9 +1,10 @@
 const express=require("express");
 const { getHealth } = require("../controllers/healthController");
-const { getUser, getRatingHistory, getSubmission } = require("../controllers/userController");
+const { getUser, getRatingHistory, getSubmission, getanalytics } = require("../controllers/userController");
 const router=express.Router();
 router.get("/",getHealth);
 router.get("/users/:handle",getUser);
 router.get("/users/:handle/rating",getRatingHistory);
-router.get("/users/:handle/submission",getSubmission)
+router.get("/users/:handle/submission",getSubmission);
+router.get("/users/:handle/analytics",getanalytics);
 module.exports=router;
