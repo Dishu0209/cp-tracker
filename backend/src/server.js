6b09a1e31e57c9a38889237d5cef92010c19ba75
Dishu <dishanksharma644@gmail.com>
2026-07-24 00:09:21 +0530

    Added start in package.json

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,7 +5,7 @@ const express=require("express");
 const connectDB=require("./config/db")
 const app=express();
 app.use(cors());
-const PORT=3000;
+const PORT = process.env.PORT || 3000;
 const routes=require("./routes")
 app.use(express.json());
 app.use("/api",routes);
